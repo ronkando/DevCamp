14b797a60edc479e782bc5dee7418f7aa989cd82
ronkando <ron@kacndo.eco>
2019-07-19 13:52:54 +0300

    portfolio updat and edit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,13 +7,29 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item =  Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
+  @portfolio_item =  Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
       else
         format.html { render :new }
+      end
+    end
+  end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title,:subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+       # format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        #format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
