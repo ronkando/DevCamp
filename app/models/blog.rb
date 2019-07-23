4c528e6161416e8a4b44b4e5fdaa32fcf44f0aef
ronkando <ron@kacndo.eco>
2019-07-23 13:42:33 +0300

    integrat topic and b;ogd

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,16 @@
 class Blog < ApplicationRecord
-  enum status: {draft: 0, published: 1 }
-
   extend FriendlyId
+
+  belongs_to :topic
+
+
+  enum status: {draft: 0, published: 1 }
   friendly_id :title, use: :slugged
+
+   validates_presence_of :title , :body
+  
   def self.my_all
     all
   end
- validates_presence_of :tile , :body
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_140441) do
+ActiveRecord::Schema.define(version: 2019_07_23_100211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2019_07_22_140441) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "card_data", force: :cascade do |t|
@@ -85,5 +87,12 @@ ActiveRecord::Schema.define(version: 2019_07_22_140441) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
   add_foreign_key "card_values", "card_data"
 end
