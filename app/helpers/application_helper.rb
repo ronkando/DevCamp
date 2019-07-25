c9b0f4cc3613cb062c5dc54de433e1df971b35f0
ronkando <ron@kacndo.eco>
2019-07-25 17:19:31 +0300

    implement source helpr with params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,11 @@ module ApplicationHelper
   def sample_helper
     "<p>My helper</p>".html_safe
   end
+
+  def source_helper(layout_name)
+    if  session[:source]
+      greeting = "Thank you visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting ,class: "source-greeting")
+    end
+  end
 end
