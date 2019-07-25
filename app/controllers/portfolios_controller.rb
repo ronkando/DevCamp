a77ffeb818393c5ce42d48ca9e0e47a21b4ed9f5
ronkando <ron@kacndo.eco>
2019-07-25 11:42:07 +0300

    controller inclued session conserens

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,8 +12,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
   def create
-  @portfolio_item =  Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body,
-                                                                     technologies_attributes: [:name]))
+  @portfolio_item =  Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -30,7 +29,7 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title,:subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
        # format.json { render :show, status: :ok, location: @blog }
       else
@@ -46,5 +45,14 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
     end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name])
   end
 end
