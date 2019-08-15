fa46997a854a6edbad0c8432e313627fcf84700b
ronkando <ron@kacndo.eco>
2019-08-15 15:18:20 +0300

    nav helper version 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,31 +2,63 @@ module ApplicationHelper
 
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to "Register" , new_user_registration_path , class: style) +
+      (link_to "Register", new_user_registration_path, class: style) +
           " ".html_safe +
-          (link_to "Login" , user_session_path , class: style)
+          (link_to "Login", user_session_path, class: style)
     else
-      link_to "Logout" , destroy_user_session_path , method: :delete , class: style
-     end
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
   end
+
   def sample_helper
     "<p>My helper</p>".html_safe
   end
 
   def source_helper(layout_name)
-    if  session[:source]
+    if session[:source]
       greeting = "Thank you visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting ,class: "source-greeting")
+      content_tag(:p, greeting, class: "source-greeting")
     end
   end
-  def nav_helper style , tag_type
-nav_link = <<VAV
-    <#{tag_type}><a href=#{root_path} class=#{style}>Home</a></#{tag_type}>
-    <#{tag_type}><a href=#{about_path} class=#{style}>About Me</a></#{tag_type}>
-    <#{tag_type}><a href=#{contact_path} class=#{style}>Contact</a></#{tag_type}>
-    <#{tag_type}><a href=#{blogs_path} class=#{style}>Blogs</a></#{tag_type}>
-    <#{tag_type}><a href=#{portfolios_path} class=#{style}>Portfolio</a></#{tag_type}>
-VAV
+
+  def nav_list_items
+    [
+        {
+            url: root_path,
+            title: 'Home'
+        },
+        {
+            url: about_path,
+            title: 'About me'
+        },
+        {
+            url: contact_path,
+            title: 'Contact'
+        },
+        {
+            url: blogs_path,
+            title: 'Blogs'
+        },
+        {
+            url: portfolios_path,
+            title: 'Portfolio'
+        }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_link = ''
+
+    nav_list_items.each do |item|
+
+      nav_link << "<#{tag_type}><a href=#{item[:url]} class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+
+    end
     nav_link.html_safe
   end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
+ 
